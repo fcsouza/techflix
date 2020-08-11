25503dc5934339c5e2ebd2de45600b9b7c491cbf
fcsouza <fcsouza18@hotmail.com>
2020-08-10 21:44:02 -0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+const Pagina404 = () => (
+  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+    <h1>Ops, quebrou tudo :O</h1>
+    <p>
+      <a href="/">Você pode jogar ou voltar pra home :)</a>
+    </p>
+    <p>
+      Ou <a href="http:">aprender a fazer o jogo</a>
+    </p>
+
+    <iframe
+      title="Flappy Bird Game"
+      src="https://flappybird.io/"
+      width="340"
+      height="600" />
+  </div>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
